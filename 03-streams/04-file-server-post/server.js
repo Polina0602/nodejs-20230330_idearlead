908c1d46e88ac783809c7b8d0165bd19127b683a
Polina0602 <idearlead@gmail.com>
2023-04-19 13:41:05 +0300

    Task3-4

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,17 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
-const server = new http.Server();
+const server = http.createServer();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      // create the files directory if it does not exist
+      if (!fs.existsSync('files')) {
+        fs.mkdirSync('files');
+      }
+        // create a new file
+        const content = 'This is my file';
+      if(fs.existsSync('./files/text.txt')){
+        res.statusCode = 409;
+        res.end('File already exists');
+      }
+        fs.writeFile('./files/text.txt', content, (err) => {
+          if (err) {
+            // handle network error
+            console.error(err);
+            // delete file if it was created
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          } else {
+            res.statusCode = 201;
+            res.end('File created');
+          }
+        });
 
       break;
 
@@ -21,4 +40,6 @@ server.on('request', (req, res) => {
   }
 });
 
-module.exports = server;
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
